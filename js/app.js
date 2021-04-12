ad95dd951b4898de4e8345cd9377e13c82bd44eb
Wiktor Jaworowski <wiktor.jaworowski25@gmail.com>
2021-04-12 14:17:35 +0200

    add functionality to contact form

--- js/app.js
+++ js/app.js
@@ -12,3 +12,42 @@ function show() {
 function close() {
     main_nav.style.top = '-100%';
 }
+
+
+
+// Listen for a submit
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+    
+    //Get input values
+    let name = document.getElementById("name").value; 
+    let email = document.getElementById("email").value; 
+    let subject = document.getElementById("subject").value; 
+    let message = document.getElementById("message").value; 
+
+    // saveContactInfo(name, email, message);
+
+    document.querySelector(".contact-form").reset();
+
+    sendEmail(name, email, subject, message);
+}
+
+//Send Email Info
+function sendEmail(name, email, subject, message) {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "email/username",
+        Password: "password",
+        To: "wiktor.jaworowski25@gmail.com",
+        From: "wiktor.jaworowski25@gmail.com",
+        Subject: `${subject}`,
+        Body: `
+        Name: ${name} <br>
+        Email-Adresse: ${email} <br>
+        Anliegen: ${subject} <br>
+        Nachricht: ${message}
+        `,
+    }).then((message) => alert("Nachricht erfolgreich gesendet"))
+}
